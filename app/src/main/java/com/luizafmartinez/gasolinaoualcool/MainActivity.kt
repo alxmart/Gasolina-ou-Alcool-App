4d6b8751382bdc7f0482b2d644d6e2f03c8aad26
alxmart <alxmartinez@gmail.com>
2023-03-04 18:07:08 -0300

    Added kotlin code to calculate.

--- app/src/main/java/com/luizafmartinez/gasolinaoualcool/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/gasolinaoualcool/MainActivity.kt
@@ -2,10 +2,37 @@ package com.luizafmartinez.gasolinaoualcool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun calcular(view: View) {
+
+        val editAlcool = findViewById<EditText>(R.id.et_preco_alcool)
+        val editGasolina = findViewById<EditText>(R.id.et_preco_gasolina)
+        val textResultado = findViewById<TextView>(R.id.tv_resultado)
+
+        val precoAlcool = editAlcool.text.toString()
+        val precoGasolina = editGasolina.text.toString()
+
+        if(precoAlcool.isNotEmpty()) {
+            if(precoGasolina.isNotEmpty()) {
+                val pAlcool = precoAlcool.toDouble()
+                val pGasolina = precoGasolina.toDouble()
+                val resultado = pAlcool / pGasolina
+
+                if (resultado >= 0.7) {
+                    textResultado.text = "Melhor usar GASOLINA"
+                } else {
+                    textResultado.text = "Melhor usar ÁLCOOL"
+                }
+            }
+        }
+   }
 }
